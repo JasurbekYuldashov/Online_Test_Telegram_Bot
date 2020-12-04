f27a4bcf0bea35887dc425088252a90fbf412ea3
Jasubek <57658965+JasurbekYuldashov@users.noreply.github.com>
2020-12-04 12:34:13 +0500

    heroku restart

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ require('dotenv').config()
 
 require("./actions")
 require("./core")
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
